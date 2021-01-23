6a798b83dd6005d2c3195aa3bda0cdae395dc91d
Abdallah <abdallahchaar90@gmail.comh>
2021-01-23 15:28:38 +0100

    commint

---
+++ my-app/src/Greeter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Greeter(props){
+    return(
+    <h1>Hi there, {props.name}your name</h1>
+    )
+}
+
+export default Greeter;
---
+++ my-app/src/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Navbar(){
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    ) 
+}
+
+export default Navbar;
