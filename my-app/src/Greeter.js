7d7180881d161bce7630a8fecf0d7d0b9eb0e5e5
Abdallah <abdallahchaar90@gmail.comh>
2021-01-23 15:37:44 +0100

    Events in React

--- my-app/src/Greeter.js
+++ my-app/src/Greeter.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-function Greeter(props){
+function Greeter({ name, excitement = 1, age = 18}){
     return(
-    <h1>Hi there, {props.name}your name</h1>
+        <>
+         <h1>Hi there, {name}{"!".repeat(excitement)}</h1>
+         <p>You are{age} years old</p>
+         <button onClick={() => alert("HELLO!")} type="button">Click Me</button>
+        </>
     )
 }
 
