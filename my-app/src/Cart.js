f8b396ca9fa19a1a2f5a01bdf37d0720633ca76b
Abdallah <abdallahchaar90@gmail.comh>
2021-01-23 18:17:17 +0100

    Update State

--- my-app/src/Cart.js
+++ my-app/src/Cart.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import CartItem from "./CartItem";
 
-function Cart({ items }) {
-return(
-    <div>
+function Cart({items}) {
+
+    const [stateItems, setItems] = useState(items);
+
+    const updateQty = (id, newQty) => {
+        const newQtyI = items.map(item => {
+            if (item.id === id){
+                return {...item, qty: newQty }
+            }
+            return item;
+        })
+        setItems(newQty);
+    }
+
+    const grandTotal = items.reduce((total, item) => (
+        total + item.qty * item.price
+    ), 0).toFixed(2);
+
+ return (
+    <div className="Cart">
         <h1> I AM CART</h1>
-        <ul>
-            {items.map(item => (
-        <CartItem{...item}/>
-        ))}
-        </ul>
+        <div className="CartItem">
+        {items.map(item => (
+          <CartItem key={item.id} updateQty={updateQty} {...item}/>
+          ))} 
+        </div>
+        <h3>Grand Total: ${grandTotal}</h3>
     </div>
   )
 }
--- my-app/src/CartItem.js
+++ my-app/src/CartItem.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 
-function CartItem ({ id, name, price, qty}) {
+function CartItem ({ id, name, price, qty, updateQty }) {
+    const addOne = () => {
+        updateQty(id, qty + 1)
+    }
+    
     return (
         <div className="CartItem">
             <div>{name}</div>
+            <div>$ {price}</div>
+            <div>
+                <button>-</button>
+                {qty}
+                <button onClick={addOne}>+</button>
+
+            </div>
+            <div>total: $ {qty * price}</div>
 
         </div>
     )
