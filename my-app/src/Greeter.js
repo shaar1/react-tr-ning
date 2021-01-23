82ddbc5b881924277ae475887101ec82d7cc4893
Abdallah <abdallahchaar90@gmail.comh>
2021-01-23 15:42:56 +0100

    Events in React onclick

--- my-app/src/Greeter.js
+++ my-app/src/Greeter.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 function Greeter({ name, excitement = 1, age = 18}){
+    const greet = () => {
+        alert("HELLO THERE; ${name}")
+    }
+
     return(
         <>
          <h1>Hi there, {name}{"!".repeat(excitement)}</h1>
          <p>You are{age} years old</p>
-         <button onClick={() => alert("HELLO!")} type="button">Click Me</button>
+         <button onClick={greet} type="button">Click Me</button>
         </>
     )
 }
 
 export default Greeter;
+
