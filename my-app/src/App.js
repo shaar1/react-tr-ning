2ae914c9f3f5bcbe4835f2336e511d5804944885
Abdallah <abdallahchaar90@gmail.comh>
2021-01-23 16:28:19 +0100

    state med count

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./navbar";
 import Greeter from "./Greeter";
+import Counter from "./Counter";
 
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Counter />
       <Navbar />
       <Greeter name="Karen" />
       <Greeter name="Tammy" />
